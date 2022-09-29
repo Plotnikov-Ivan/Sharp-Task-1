add545b984670d410a6cd7e08c642af791ad92d7
User <User@WIN-QM98R3SV0AI>
2022-09-29 23:01:36 +0300

    Добавьте файлы проекта.

---
+++ Sharp Task 1/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿
+namespace Sharp_Task_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 379);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(192, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(18, 197);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(193, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(43, 140);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(318, 140);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(105, 20);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(18, 67);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(106, 22);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Задание 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(531, 67);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(106, 22);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Задание 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Число слагаемых ( для задания 1)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(335, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Аргумент";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(15, 360);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Частичная сумма ряда";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(173, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(410, 31);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "1/(1+x) = 1 - x + x^2 - x^3 + x^4...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(15, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Точное значение функции";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 16);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Последнее слагаемое";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(283, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 62);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(15, 321);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(193, 20);
+            this.textBox6.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(15, 292);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(175, 16);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Абсолютная погрешность";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 259);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(18, 416);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(388, 20);
+            this.textBox7.TabIndex = 22;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(558, 140);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(110, 20);
+            this.textBox8.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(528, 104);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(164, 16);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Число Е (для задания 2)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(412, 178);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(376, 16);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Сумма членов ряда по абсолютной величине больших Е";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(558, 211);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(168, 20);
+            this.textBox9.TabIndex = 26;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(558, 259);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(168, 20);
+            this.textBox10.TabIndex = 27;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(564, 240);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(150, 16);
+            this.label10.TabIndex = 28;
+            this.label10.Text = "Количество членов N";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(412, 292);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(379, 16);
+            this.label11.TabIndex = 29;
+            this.label11.Text = "Сумма членов ряда по абсолютной величине больших Е\'";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(433, 432);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(341, 16);
+            this.label12.TabIndex = 30;
+            this.label12.Text = "Е\' - число на порядок (в 10 раз) отличающееся от Е";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(558, 311);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(168, 20);
+            this.textBox11.TabIndex = 31;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 457);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox11;
+    }
+}
+
---
+++ Sharp Task 1/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sharp_Task_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton1 = (RadioButton)sender;
+
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton2 = (RadioButton)sender;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                task1();
+            }
+
+            if (radioButton2.Checked)
+            {
+                task2();
+            }
+        }
+        public void task1()
+        {
+            int N = Convert.ToInt32(textBox4.Text); //кол-во слагаемых
+            int x = Convert.ToInt32(textBox5.Text); // аргумент
+
+            textBox2.Text = (frac(N, x)).ToString(); //частичная сумма ряда
+            textBox3.Text = (last(N, x)).ToString(); //последнее слагаемое
+            textBox1.Text = (perf(x)).ToString(); //точное значение функции
+            textBox6.Text = (pogr(frac(N, x), perf(x))).ToString(); //абсолютная погрешность
+            textBox7.Text = (sravn((pogr(frac(N, x), perf(x))), (last(N, x))).ToString());
+        }
+
+        public void task2()
+        {
+            double E = Convert.ToDouble(textBox8.Text); //кол-во слагаемых
+            if (E > 1)
+            {
+                MessageBox.Show("Ошибка! Число Е должно быть меньше 1");
+            }
+            double x = Convert.ToDouble(textBox5.Text); // аргумент
+
+            textBox9.Text = (frac1(E, x)).ToString(); //Сумма членов ряда по абсолютной величине больших Е
+            textBox10.Text = (kolvo(E, x)).ToString(); //Кол-во членов ряда
+            textBox11.Text = (fracE2(E, x)).ToString();//Сумма членов ряда по абсолютной величине больших Е'
+        }
+
+        public double frac(int N, int x)
+        {
+
+            double sum = 1;
+
+            for (int i = 1; i < N; i++)
+            {
+                sum += Math.Pow(-x, i);
+
+            }
+            return sum;
+        }
+        public double frac1(double E, double x)
+        {
+
+            double sum = 0;
+            int count = 0;
+            int count1 = 0;
+            for (int i = 0;  ; i++)
+            {
+                double v = Math.Pow(-x, i);
+                if (Math.Abs(v) > E)
+                {
+                    sum += Math.Pow(-x, i);
+                    count ++;
+                }
+                count1++;
+                if (count != count1)
+                {
+                    break;
+                }
+            }
+            return sum;
+        }
+
+        public double kolvo(double E, double x)
+        {
+
+            double sum = 0;
+            int count = 0;
+            int count1 = 0;
+            for (int i = 0;  ; i++)
+            {
+                double v = Math.Pow(-x, i);
+                if (Math.Abs(v) > E)
+                {
+                    sum += Math.Pow(-x, i);
+                    count++;
+                }
+                count1++;
+                if (count != count1)
+                {
+                    break;
+                }
+            }
+            return count;
+        }
+
+        public double fracE2(double E, double x)
+        {
+
+            double sum = 0;
+            int count = 0;
+            int count1 = 0;
+            for (int i = 0; ; i++)
+            {
+                double v = Math.Pow(-x, i);
+                if (Math.Abs(v) > E/10)
+                {
+                    sum += Math.Pow(-x, i);
+                    count++;
+                }
+                count1++;
+                if (count != count1)
+                {
+                    break;
+                }
+            }
+            return sum;
+        }
+
+        public double perf(double x)
+        {
+            double perf = 1 / (1 + x);
+            return perf;
+        }
+
+        public double pogr(double sum, double perf)
+        {
+            double p = Math.Abs(perf - sum);
+            return p;
+        }
+
+        public double last(int x, int N)
+        {
+            double last = Math.Pow(-x, N);
+            return last;
+        }
+
+        public string sravn(double p, double last)
+        {
+            string с;
+            if (p < Math.Abs(last))
+            {
+                string m = "Абсолютная погрешность меньше абсолютной велечины последнего члена";
+                return m;
+            }
+
+            if (p > Math.Abs(last))
+            {
+                string m = "Абсолютная погрешность больше абсолютной велечины последнего члена";
+                return m;
+            }
+
+            if (p == Math.Abs(last))
+            {
+                string m = "Абсолютная погрешность равна абсолютной велечины последнего члена";
+                return m;
+            }
+            else return с = "Ошибка";
+        }
+
+    
+    }
+}
